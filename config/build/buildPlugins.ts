4c9921e76331262af3d51240037d0eeb472c7aa7
Eugene Myagkih <myagkikh.e@gmail.com>
2023-02-28 00:04:52 +0700

    adding WBA plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,8 @@
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
 import { type BuildOptions } from './types/config';
 
 export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => ([
@@ -11,6 +13,9 @@ export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPlu
   new MiniCssExtractPlugin({
     'filename': 'css/[name][contenthash:8].css',
     'chunkFilename': 'css/[name][contenthash:8].css',
+  }),
+  new BundleAnalyzerPlugin({
+    'openAnalyzer': false,
   }),
   new webpack.DefinePlugin({
     '__DEV__': JSON.stringify(isDev),
