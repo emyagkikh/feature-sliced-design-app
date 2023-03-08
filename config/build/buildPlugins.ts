4ffbe061dac5857f02a56bc04ba327475507427a
Eugene Myagkih <myagkikh.e@gmail.com>
2023-03-08 23:45:57 +0700

    fix webpack plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,19 +5,39 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 import { type BuildOptions } from './types/config';
 
-export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => ([
-  new webpack.ProgressPlugin(),
-  new HtmlWebpackPlugin({
-    'template': paths.html,
-  }),
-  new MiniCssExtractPlugin({
-    'filename': 'css/[name][contenthash:8].css',
-    'chunkFilename': 'css/[name][contenthash:8].css',
-  }),
-  new BundleAnalyzerPlugin({
-    'openAnalyzer': false,
-  }),
-  new webpack.DefinePlugin({
-    '__DEV__': JSON.stringify(isDev),
-  }),
-]);
+export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => {
+  const pluginsCommon: webpack.WebpackPluginInstance[] = [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      'template': paths.html,
+    }),
+  ];
+
+  const pluginsDev: webpack.WebpackPluginInstance[] = [
+    new BundleAnalyzerPlugin({
+      'openAnalyzer': false,
+    }),
+    new webpack.DefinePlugin({
+      '__DEV__': JSON.stringify(isDev),
+    }),
+  ];
+
+  const pluginsProd: webpack.WebpackPluginInstance[] = [
+    new MiniCssExtractPlugin({
+      'filename': 'css/[name][contenthash:8].css',
+      'chunkFilename': 'css/[name][contenthash:8].css',
+    }),
+  ];
+
+  let pluginsTotal: webpack.WebpackPluginInstance[] = [];
+
+  if (isDev) {
+    pluginsTotal = pluginsCommon.concat(pluginsDev);
+  }
+
+  if (!isDev) {
+    pluginsTotal = pluginsCommon.concat(pluginsProd);
+  }
+
+  return pluginsTotal;
+};
