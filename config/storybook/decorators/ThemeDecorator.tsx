77d79780a02168b30669bf29967930cef2bc3159
Eugene Myagkih <myagkikh.e@gmail.com>
2023-03-08 23:27:00 +0700

    fix part of eslint errors

--- config/storybook/decorators/ThemeDecorator.tsx
+++ config/storybook/decorators/ThemeDecorator.tsx
@@ -1,6 +1,7 @@
 import { type ThemeEnum, ThemeProvider } from 'app/providers/ThemeProvider';
 import { type Story } from '@storybook/react';
 
+// eslint-disable-next-line react/display-name
 export const ThemeDecorator = (theme: ThemeEnum) => (StoryComponent: Story) => (
   <div className={`app ${theme}`}>
     <ThemeProvider>
